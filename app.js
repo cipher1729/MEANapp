bc6c7a541e46ae3f187d7c8a4747b3ef0b460753
cipher1729 <cipher1729@gmail.com>
2015-09-21 02:16:32 -0700

    Added factory

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 var app = angular.module('flapperNews',[]);
-app.controller('MainCtrl',['$scope', function($scope){
+app.controller('MainCtrl',['$scope','posts',  function($scope, posts){
 $scope.test = 'Hello World!';
-$scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 5', upvotes: 4}
-];
+$scope.posts = posts.posts;
 
 $scope.addPost = function(){
 	if(!$scope.title || $scope.title==='') {return;}
-	$scope.posts.push({title:$scope.title, link:$scope.link, upvotes:0});
+	$scope.posts.push({title:$scope.title, link:	$scope.link, upvotes:0});
 	$scope.title='';
 	$scope.link='';
 }
@@ -21,6 +15,11 @@ $scope.clickUpvote= function(post)
 	post.upvotes =post.upvotes+1;
 }
 }]);
+
+app.factory('posts', function (){
+	var o ={posts:[{'Aloha':4}]};
+	return o;
+});
 
 
 
